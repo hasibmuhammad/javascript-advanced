a62d288ff55723f3a36cbc7df69e710a53a8799c
hasibmu99 <hasibmu99@gmail.com>
2020-03-04 22:14:44 +0600

    double equal vs tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,18 @@
+let firstNumber = 1;
+let secondNumber = '1';
+
+let firstNumber = true;
+let secondNumber = true;
+
+let firstNumber = 1;
+let secondNumber = '1';
+
+let firstNumber = false
+let secondNumber = false;
+
+if (firstNumber === secondNumber) {
+    const result = firstNumber + secondNumber;
+    console.log("Condition is true and result is " + result);
+} else {
+    console.log("Condition is false.");
+}
