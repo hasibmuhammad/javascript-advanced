62724288e7928b1bfc03a192aaf37bc7de22f72b
hasibmu99 <hasibmu99@gmail.com>
2020-03-05 08:05:04 +0600

    Another practice using arguments

--- arguments.js
+++ arguments.js
@@ -4,6 +4,12 @@ function add() {
         sum += arguments[i];
     }
     return sum;
+
+    let sum = 0;
+    [...arguments].map( (element) => {
+        sum += element;
+    });
+    return sum;
 }
 const result = add(2, 3, 6, 7, 9, 9, 9, 9, 9, 9);
 console.log(result);
